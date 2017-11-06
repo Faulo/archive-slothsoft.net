be94e1db4abdee5b8ae80efe5c8988597b28357f
FauloLio <Daniel_Schulz1@web.de>
2017-11-06 19:07:19 +0100

    amber global

---
+++ mod/savegame/php/Node/GlobalNode.php
@@ -0,0 +1,26 @@
+<?php
+namespace Slothsoft\Savegame\Node;
+
+declare(ticks = 1000);
+
+class GlobalNode extends AbstractNode
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->strucData['global-id'] = '';
+    }
+    protected function loadNode() {
+        
+    }
+    protected function loadChildren()
+    {
+        
+    }
+    public function asXML() {
+        return $this->getChildrenXML();
+    }
+    public function getGlobalId() {
+        return $this->strucData['global-id'];
+    }
+}
